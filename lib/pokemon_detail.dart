18ef614be656d846edccd32ebdddac6dff20ae66
DESKTOP-HOE4206\shy_z <akbeta08@gmail.com>
2020-11-03 23:22:31 +0300

    pokemon specialities added

--- lib/pokemon_detail.dart
+++ lib/pokemon_detail.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:pokedex/model/pokedex.dart';
 
@@ -9,7 +10,7 @@ class PokemonDetail extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.deepOrange,
+      backgroundColor: Colors.orange.shade200,
       appBar: AppBar(
         elevation: 0,
         title: Text(
@@ -29,7 +30,68 @@ class PokemonDetail extends StatelessWidget {
               shape: RoundedRectangleBorder(
                 borderRadius: BorderRadius.circular(14),
               ),
-              child: Text(pokemon.name),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  SizedBox(
+                    height: 60,
+                  ),
+                  Text(
+                    pokemon.name,
+                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                  ),
+                  Text(
+                    "Height: " + pokemon.height,
+                    style: TextStyle(fontSize: 16),
+                  ),
+                  Text(
+                    "Weight: " + pokemon.weight,
+                    style: TextStyle(fontSize: 16),
+                  ),
+                  Text(
+                    "Weaknesses: ",
+                    style: TextStyle(fontSize: 16),
+                  ),
+                  Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: pokemon.weaknesses != null
+                          ? pokemon.weaknesses
+                              .map((weakness) => Chip(
+                                    backgroundColor: Colors.red.shade300,
+                                    label: Text(
+                                      weakness,
+                                      style: TextStyle(color: Colors.white),
+                                    ),
+                                  ))
+                              .toList()
+                          : [Text(pokemon.name + " has no weakness")]),
+                  Text(
+                    "Next Evelution: ",
+                    style: TextStyle(fontSize: 16),
+                  ),
+                  Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: pokemon.nextEvolution != null
+                          ? pokemon.nextEvolution
+                              .map((evelution) => Chip(
+                                    backgroundColor: Colors.red.shade300,
+                                    label: Text(
+                                      evelution.name,
+                                      style: TextStyle(color: Colors.white),
+                                    ),
+                                  ))
+                              .toList()
+                          : [
+                              Text(
+                                pokemon.name + " does not evolve any more",
+                                style: TextStyle(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.red.shade300),
+                              )
+                            ]),
+                ],
+              ),
             ),
           ),
           Align(
@@ -37,8 +99,8 @@ class PokemonDetail extends StatelessWidget {
             child: Hero(
                 tag: pokemon.img,
                 child: Container(
-                  width: 200,
-                  height: 200,
+                  width: 150,
+                  height: 150,
                   child: Image.network(
                     pokemon.img,
                     fit: BoxFit.contain,
